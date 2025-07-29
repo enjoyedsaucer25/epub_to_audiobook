bd9554277971ec798a9a49b5be2bb57d089d30ec
enjoyedsaucer25 <149631152+enjoyedsaucer25@users.noreply.github.com>
2025-07-29 12:53:09 -0400

    Update main_ui.py

--- main_ui.py
+++ main_ui.py
@@ -1,3 +1,4 @@
+import os
 import argparse
 
 from audiobook_generator.config.ui_config import UiConfig
@@ -6,8 +7,13 @@ from audiobook_generator.ui.web_ui import host_ui
 
 def handle_args():
     parser = argparse.ArgumentParser(description="WebUI for Epub to Audiobook convertor")
-    parser.add_argument("--host", default="127.0.0.1", help="Host address")
-    parser.add_argument("--port", default=7860, type=int, help="Port number")
+    
+    # Read from environment or default to localhost
+    port = int(os.environ.get("PORT", 7860))
+    host = os.environ.get("HOST", "0.0.0.0")
+
+    parser.add_argument("--host", default=host, help="Host address")
+    parser.add_argument("--port", default=port, type=int, help="Port number")
 
     ui_args = parser.parse_args()
     return UiConfig(ui_args)
